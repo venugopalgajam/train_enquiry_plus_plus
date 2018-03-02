aaf0af338d08833d6eed67aa2c07cbb5d956fe51
Venu Gopal Gajam <Venu Gopal Gajam>
2018-03-02 21:31:50 +0530

    consider next day for pairing

--- TEPP.py
+++ TEPP.py
@@ -15,19 +15,20 @@ def get_trainspair(src, intr, dst, week_day):
 
     result_list =list()
     for tpl in list1:
-        second_train_day = week_day + tpl[-1] - tpl[-2] + int(tpl[-4] > tpl[-3])
-        if week_day_2 != second_train_day:
-            list2 = trains_btw_with_times(intr, dst, second_train_day)
-            week_day_2 = second_train_day
-        if list2 is None or len(list2) <= 0:
-            print('no train btw',intr, src)
-            continue
-        for tpl2 in list2:
-            if tpl[-4] < tpl[-3]:
-                if tpl2[4] > tpl[5]:
-                    result_list.append([[tpl[0],week_day],[tpl2[0],week_day_2]])
-            else:
-                result_list.append([[tpl[0],week_day],[tpl2[0],week_day_2]])
+        second_train_day = (week_day + tpl[-1] - tpl[-2] + int(tpl[-4] > tpl[-3]))%7
+        for offset in range(2):
+            if week_day_2 != second_train_day:
+                week_day_2 = (second_train_day +offset)%7
+                list2 = trains_btw_with_times(intr, dst,week_day_2)
+            if list2 is None or len(list2) <= 0:
+                print('no train btw',intr, src)
+                continue
+            for tpl2 in list2:
+                if tpl[-4] < tpl[-3]:
+                    if tpl2[4] > tpl[5]:
+                        result_list.append([[tpl,week_day],[tpl2,week_day_2]])
+                else:
+                    result_list.append([[tpl,week_day],[tpl2,week_day_2]])
     return result_list
     
 def get_paths(src, dst, jdate):
@@ -39,9 +40,17 @@ def get_paths(src, dst, jdate):
     if stns_list is None:
         print('no intermediate stations',src,dst)
         return None
-    for intr in stns_list:
-        print(intr[0])
-        for row in get_trainspair(src,intr[0],dst,week_day):
-            print(row)
+    # for intr in stns_list:
+    #     print(intr[0])
+    train_pairs = get_trainspair(src,'VTM',dst,week_day)
+    print(len(train_pairs))
+    for row in train_pairs:
+        print(row[0][0][0],row[0][1],row[1][0][0],row[1][1])
+        # print(row[1])
+        # print(row[0][0])
+        # print(row[0][0][0],src,'BZA',jdate+dt.timedelta(days=(row[0][1]-week_day+7)%7)+row[0][0][4],jdate+dt.timedelta(days=(row[0][1]-week_day+7)%7)+row[0][0][5])
+        # print(row[1][0][0],'BZA',dst,jdate+dt.timedelta(days=(row[0][1]-week_day+7)%7)+row[1][0][4],jdate+dt.timedelta(days=(row[0][1]-week_day+7)%7)+row[1][0][5])
+        print(' ')
 
-get_paths('MAS', 'NDLS',dt.datetime(2018,3,4))
+get_paths('KZJ', 'OGL',dt.datetime(2018,3,4))
+# print(trains_btw('MAS','BZJ','SUN'))
